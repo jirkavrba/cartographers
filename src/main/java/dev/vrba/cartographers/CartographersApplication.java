b30c4eb0365bc79cccb5f609db67639e3c3e79f6
Jiří Vrba <jiri@vrba.dev>
2021-02-07 18:22:52 +0100

    Initial commit, fresh spring boot installation

---
+++ src/main/java/dev/vrba/cartographers/CartographersApplication.java
@@ -0,0 +1,13 @@
+package dev.vrba.cartographers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartographersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CartographersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/vrba/cartographers/CartographersApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vrba.cartographers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartographersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
