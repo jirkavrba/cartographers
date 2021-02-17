57b82c2ccd90351f18a9f5ffeed0f2cc2ff99a62
Jiří Vrba <jiri@vrba.dev>
2021-02-17 13:51:43 +0100

    Added login screen to client application

--- src/client/src/store/index.js
+++ src/client/src/store/index.js
@@ -5,10 +5,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    id: localStorage.getItem("id") || null,
+    username: localStorage.getItem("username") || null,
+    game: null
   },
   mutations: {
+    login(state, payload) {
+        state.id = payload.id
+        state.username = payload.username
+    }
   },
   actions: {
+    login(payload) {
+      let id = (Math.random() * (1 << 16)).toString(36)
+      let username = payload.username
+
+      this.commit('login', {id, username})
+    }
   },
   modules: {
   }
---
+++ src/client/src/views/Welcome.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <h2>Welcome! Please choose an username and click next</h2>
+
+    <label for="username">Username:</label>
+    <input type="text" id="username" v-model="username">
+    <small>Username must have at least 3 characters</small>
+
+    <hr>
+
+    <button @click="login" :disabled="username.length < 3">Next</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Welcome',
+  data: () => ({
+    username: ""
+  }),
+  methods: {
+    login() {
+      this.$router.replace('/games');
+      this.$store.dispatch('login', this.username)
+    }
+  },
+  components: {}
+}
+</script>
