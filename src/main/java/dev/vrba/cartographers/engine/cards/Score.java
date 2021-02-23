a952d06ec980dcff6fd25ce1e840225e5eff7c8a
Jiří Vrba <jiri@vrba.dev>
2021-02-23 12:30:59 +0100

    Added Score data class to be evaluated at the end of every season

---
+++ src/main/java/dev/vrba/cartographers/engine/cards/Score.java
@@ -0,0 +1,15 @@
+package dev.vrba.cartographers.engine.cards;
+
+import lombok.Data;
+
+@Data
+public class Score {
+
+    public final int firstEdictCardEvaluation;
+
+    public final int secondEdictCardEvaluation;
+
+    public final int acquiredCoins;
+
+    public final int ambushedTilesLoss;
+}
