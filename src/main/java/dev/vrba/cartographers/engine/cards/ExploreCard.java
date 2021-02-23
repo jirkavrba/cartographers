fa0f846dd53fdde8e2df907881f5781e1923de20
Jiří Vrba <jiri@vrba.dev>
2021-02-23 12:25:47 +0100

    Moved ExploreCardVariant under ExploreCard as a static class

--- src/main/java/dev/vrba/cartographers/engine/cards/ExploreCard.java
+++ src/main/java/dev/vrba/cartographers/engine/cards/ExploreCard.java
@@ -1,10 +1,19 @@
 package dev.vrba.cartographers.engine.cards;
 
+import dev.vrba.cartographers.engine.Material;
+import dev.vrba.cartographers.engine.map.Shape;
 import lombok.Data;
 
 @Data
 public class ExploreCard {
     private final String name;
-    private final ExploreCardVariant[] variants;
+    private final ShapeVariant[] variants;
     private final int timeValue;
+
+    @Data
+    public static class ShapeVariant {
+        private final Shape shape;
+        private final Material material;
+        private final boolean coinReward;
+    }
 }
